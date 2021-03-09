473f82ed12ca47ee89c163fad575f98770a59745
snehal0307 <76777620+snehal0307@users.noreply.github.com>
2021-03-09 12:18:16 +0530

    Create Positiveno.py

---
+++ Positiveno.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+print(list1)
+for n in list1:
+    if n>=0:
+        print(n,end=" ")
+
+print("\n")
+list2=[12,14,-95,3]
+print(list2)
+for num in list2:
+    if num>=0:
+        print(num,end=" ")
